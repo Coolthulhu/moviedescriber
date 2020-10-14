734a529cbbb16bfb64f22f7995ffa177b14b255d
Coolthulhu <Coolthulhu@gmail.com>
2020-10-14 17:43:58 +0200

    Aggressive logging

--- thesite/thesite/settings.py
+++ thesite/thesite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,8 +107,10 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'strm': sys.stdout
         },
     },
     'root': {
